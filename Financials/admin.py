d0b0641a9b023eca654e0223d6f02d5dafba0f27
brihyn-unomaha <35381257+brihyn-unomaha@users.noreply.github.com>
2018-02-14 12:17:17 -0600

    added models, created admin users

--- Financials/admin.py
+++ Financials/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Customer
+from .models import Stock
+from .models import Cryptocurrency
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Stock)
+admin.site.register(Cryptocurrency)
--- Financials/models.py
+++ Financials/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    streetaddress = models.CharField(max_length=100)
+    city = models.CharField(max_length=500)
+    state = models.CharField(max_length=2)
+    zip = models.CharField(max_length=5)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=12)
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    modified_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def save(self):
+        self.modified_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Stock(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    symbol = models.CharField(max_length=10)
+    qty = models.IntegerField
+    purchaseprice = models.FloatField
+    purchasedate = models.DateTimeField(
+            default=timezone.now)
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    modified_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def save(self):
+        self.modified_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Cryptocurrency(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    qty = models.IntegerField
+    purchaseprice = models.FloatField
+    purchasedate = models.DateTimeField(
+        default=timezone.now)
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    modified_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def save(self):
+        self.modified_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
