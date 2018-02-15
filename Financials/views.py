03c72aaaadd3d1b194363258bfec4d20dd82cf17
brihyn-unomaha <35381257+brihyn-unomaha@users.noreply.github.com>
2018-02-15 12:51:06 -0600

    on "Intro to HTML" section
    created empty HTML pages, Urls.py, and updated Views.py

--- Financials/views.py
+++ Financials/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def financial_list(request):
+    return render(request, 'Financials/financial_list.html', {})
+
+def customer_list(request):
+    return render(request, 'Financials/customer_list.html', {})
+
+def crypto_list(request):
+    return render(request, 'Financials/crypto_list.html', {})
+
+def stock_list(request):
+    return render(request, 'Financials/stock_list.html', {})
