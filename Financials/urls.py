9118dac100878a7653b3adaea69e110205ce8fe7
brihyn-unomaha <35381257+brihyn-unomaha@users.noreply.github.com>
2018-02-18 20:48:58 -0600

    Closing in
    CSS Finished
    Customer Detail needs to have stock/crypto
    Update Stock and Crypto Detail pages

--- Financials/urls.py
+++ Financials/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.customer_list, name='customer_list'),
-    url(r'^crypto/(P<pk>\d+)/$', views.crypto_list, name='crypto_list'),
-    url(r'^stock/(P<pk>\d+)/$', views.stock_list, name='stock_list'),
+    url(r'^customer/(?P<pk>\d+)/$', views.customer_detail, name='customer_detail'),
+    url(r'^stock/(?P<pk>\d+)/$', views.stock_detail, name='stock_detail'),
+    url(r'^crypto/(?P<pk>\d+)/$', views.crypto_detail, name='crypto_detail'),
 ]
--- Financials/views.py
+++ Financials/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Customer
 from .models import Stock
 from .models import Cryptocurrency
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -9,10 +10,14 @@ def customer_list(request):
     customers = Customer.objects.order_by('customer_name')
     return render(request, 'Financials/customer_list.html', {'customers':customers})
 
-def crypto_list(request):
-    crypto = Cryptocurrency.objects.order_by('crypto_name')
-    return render(request, 'Financials/crypto_list.html', {'crypto':crypto})
+def customer_detail(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    return render(request, 'Financials/customer_detail.html', {'customer': customer})
 
-def stock_list(request):
-    stocks = Stock.objects.order_by('stock_name')
-    return render(request, 'Financials/stock_list.html', {'stocks':stocks})
+def stock_detail(request, pk):
+    stock = get_object_or_404(Stock, pk=pk)
+    return render(request, 'Financials/stock_detail.html', {'stock': stock})
+
+def crypto_detail(request, pk):
+    cryptocurrency = get_object_or_404(Cryptocurrency, pk=pk)
+    return render(request, 'Financials/cryptocurrency_detail.html', {'cryptocurrency': cryptocurrency})
